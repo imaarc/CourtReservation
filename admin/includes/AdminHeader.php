b031a520e51cb99729d468273fff82e11fea399a
John Paul Eltanal <poleltanal@gmail.com>
2024-01-14 23:35:37 +0800

    commit bago liwat

--- admin/includes/AdminHeader.php
+++ admin/includes/AdminHeader.php
@@ -55,7 +55,7 @@
 		            <div class="app-utilities col-auto">
 			            
 			            <div class="app-utility-item app-user-dropdown dropdown">
-				            <a class="dropdown-toggle" id="user-dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><img src="assets/images/user.png" alt="user profile"></a>
+				            <a class="dropdown-toggle" id="user-dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><img src="assets/images/userdocs.jpg" alt="user profile"></a>
 				            <ul class="dropdown-menu" aria-labelledby="user-dropdown-toggle">
 								<li><a class="dropdown-item" href="account.html">Account</a></li>
 								<li><a class="dropdown-item" href="settings.html">Settings</a></li>
